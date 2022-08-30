b05d81dff97a0ae0900158134d5f5d3f923b4fa0
LuliGod <luligodio@gmail.com>
2022-08-30 19:30:09 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     ThemeModule.forRoot({
       themes: [lightTheme, darkTheme],
-      active: 'darktheme'
+      active: 'darkTheme'
     })],
   providers: [],
   bootstrap: [AppComponent],
