42bfb9f8678848e08368d8c57ccd12a911d35314
LuliGod <luligodio@gmail.com>
2022-08-30 18:48:55 -0300

    Armando el login y register
    pasaron muchas cosas aca pero ahora esta todo bien

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// themes
+import { ThemeModule } from './styles/themes/theme.module';
+import { lightTheme } from './styles/themes/light-theme';
+import { darkTheme } from './styles/themes/dark-theme';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentsModule } from './shared/modules/components/components.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    ComponentsModule,
+    BrowserAnimationsModule,
+    ThemeModule.forRoot({
+      themes: [lightTheme, darkTheme],
+      active: 'darktheme'
+    })],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
